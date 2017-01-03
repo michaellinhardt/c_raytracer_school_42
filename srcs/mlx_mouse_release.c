fb4d16d453f432b3b6ab9b93a2607792044e976f
Michael Linhardt <nestoyeur@gmail.com>
2017-01-04 00:50:11 +0100

    fix click event

--- srcs/mlx_mouse_release.c
+++ srcs/mlx_mouse_release.c
@@ -9,12 +9,12 @@ void	mouse_release(t_mlx *m, int btn, int x, int y)
 	i = -1;
 	while (m->scene_img[m->scene][++i].img)
 	{
-		if ((img = &m->scene_img[m->scene][i])->status > IMG_ANIM
+		if (((img = &m->scene_img[m->scene][i])->status == MENU
+		|| img->status == OBJECT)
 		&& x >= img->mouse.top[0] && x <= img->mouse.bot[0]
 		&& y >= img->mouse.top[1] && y <= img->mouse.bot[1]
 		&& (btn == img->mouse.btn || img->mouse.btn == 3))
 		{
-			ft_printf("click\n");
 			if (img->mouse.click_id > 0)
 				layer_add(m, layer(m, 3, 0)
 					, &m->scene_img[m->scene][img->mouse.click_id]);
