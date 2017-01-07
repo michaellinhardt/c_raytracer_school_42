6a8e5f32815beb9f2df197dc209abc0c26375f34
Michael Linhardt <nestoyeur@gmail.com>
2017-01-08 00:42:29 +0100

    fix mouse over when menu isnt open

--- srcs/mlx_mouse_over.c
+++ srcs/mlx_mouse_over.c
@@ -12,7 +12,7 @@ void	mouse_over(t_mlx *m, int x, int y)
 		if ((img = &m->scene_img[m->scene][i])->status > DISABLED
 		&& img->mouse.over_id > 0 && area(img, NULL, x, y))
 		{
-			if (img->status == MENU && (m->menu.draw == 0
+			if (img->status == MENU && (m->menu.draw == -1
 			|| (m->menu.draw == 1 && img->menu != NONE
 			&& img->menu != m->menu.id)))
 				continue ;
