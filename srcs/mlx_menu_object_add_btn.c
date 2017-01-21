56510fed88dc85b47a441f90367121dd45553cf5
Michael Linhardt <nestoyeur@gmail.com>
2017-01-21 14:15:18 +0100

    new object ok

--- srcs/mlx_menu_object_add_btn.c
+++ srcs/mlx_menu_object_add_btn.c
@@ -4,21 +4,25 @@ void			menu_object_add_btn_ok(void *gen, void *mlx)
 {
 	t_mlx	*m;
 	t_gen	*d;
+	t_obj	*obj;
 
 	m = mlx;
 	d = gen;
-	return ;
-	if (!m->menu.new_scene || !ft_strcmp(m->menu.new_scene
-	, TXT_SCENE_ADD_NAME_DEFAUT))
-		notif(m, N_WARNING, "you must enter a new scene name");
-	else if (ft_strlen(m->menu.new_scene) < 3)
-		notif(m, N_WARNING, "scene name must have at least 3 char");
+	if (!m->menu.new_object || !ft_strcmp(m->menu.new_object
+	, TXT_OBJECT_ADD_NAME_DEFAUT))
+		notif(m, N_WARNING, "you must enter a new object name");
+	else if (ft_strlen(m->menu.new_object) < 3)
+		notif(m, N_WARNING, "object name must have at least 3 char");
 	else
 	{
-		parse_scene(d, PATH_NEW_SCENE);
-		ft_strdel(&d->sc->name);
-		d->sc->name = ft_strdup(m->menu.new_scene);
-		d->sc->path_save = NULL;
-		m->menu.draw = -1;
+		if (!(obj = (t_obj *)ft_memalloc(sizeof(t_obj))))
+			error(2, "ft_memalloc(t_obj)");
+		ft_memcpy(obj, d->sc->obj, sizeof(t_obj));
+		obj->name = m->menu.new_object;
+		obj->type = SPHERE;
+		m->menu.new_object = ft_strdup(TXT_OBJECT_ADD_NAME_DEFAUT);
+		obj->next = d->sc->obj;
+		d->sc->obj = obj;
+		menu_object_open_order(d, m);
 	}
 }
--- srcs/mlx_menu_object_clic.c
+++ srcs/mlx_menu_object_clic.c
@@ -3,7 +3,6 @@
 
 void		menu_object_clic(t_gen *d, t_mlx *m, t_flst *elem)
 {
-	ft_printf("load object: %s\n", elem->path);
 	m->menu.obj = elem->obj;
 	m->menu.id = LOAD_OBJECT;
 	menu_edit_mod_btn_open(d, m);
