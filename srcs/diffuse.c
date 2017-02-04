4521c86e451f06d346843b4ab02ebb9f4ada6686
Benoit BRUNELLE <bbrunell@e1r11p5.42.fr>
2017-02-04 15:04:55 +0100

    c bon

--- srcs/diffuse.c
+++ srcs/diffuse.c
@@ -43,12 +43,13 @@ t_obj *tmp)
 		c->vec_obj_light = vectormultby_scalar(vector_normalize(
 			new_vector(c->spot->pos[3], c->spot->pos[4], c->spot->pos[5])), -1);
 	c->dot_light_norm = vector_dot(c->vec_obj_light, r->norm);
-	c->i_l[c->i] = (ft_shadow(sc->obj, c, sc) == 0) ? c->i_l[c->i] : 0;
+	c->i_l[c->i] = ft_shadow(sc->obj, c, sc) * c->i_l[c->i];
+	c->i_l[c->i] = (c->i_l[c->i] < 0) ? 0 : c->i_l[c->i];
 	if (vector_dot(r->norm, vectormultby_scalar(c->vec_obj_light, -1)) > 0 &&
 	tmp->type == PLAN)
 		return (0);
 	diffuse_shadow_specular_end(c, r, &speculaire, &obj_dotn);
-	return (c->i_l[c->i] = 0 ? 0 : c->i_l[c->i] * (c->rgb[c->i] *
+	return ((c->i_l[c->i] == 0) ? 0 : c->i_l[c->i] * (c->rgb[c->i] *
 	c->dot_light_norm + 1 * pow(speculaire, 66)));
 }
 
