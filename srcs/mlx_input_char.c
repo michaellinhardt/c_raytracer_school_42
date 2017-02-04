306344fb05d8b71d74867ebe7161af51b061ef4a
Michael Linhardt <nestoyeur@gmail.com>
2017-02-04 09:22:12 +0100

    you can now validate an input field with the twi enter key

--- srcs/mlx_input_char.c
+++ srcs/mlx_input_char.c
@@ -5,13 +5,15 @@ void			input_catch_char(t_gen *d, t_mlx *m, t_get *g, int key)
 {
 	char		c;
 
+	ft_printf("key: %d\n", key);
+
 	if (key == 123 || key == 124)
 		cursor_move_left_right(&d->mlx, m->getfocus, 666);
 	else if (key == 51)
 		input_delete_char(m->getfocus, m->getfocus->i - 1, 666);
 	else if (key == 117)
 		input_delete_char(m->getfocus, m->getfocus->i, 666);
-	else if (key == 36)
+	else if (key == 36 || key == 76)
 		g->action = GET_VALID;
 	else
 	{
